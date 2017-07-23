61e2cf4a42365ed7bb9eaf88f8e93e9ab69af848
snsun <snsun@nwpu-aslp.org>
2017-07-23 08:43:06 +0800

    Add path file. Debug the fix the run.sh, step1

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@
 #      We define the tf records format for our task, please see the codes for the details
 #   3. Traing & Test model: Tensorflow
 
-step=0
+step=1
 #Step 0: extract features using matlab program. 
 #    Note: You need to change the data_dir path and feats_dir path in 
 #          matlab_feats_extraction/extract_czt_fft_feats.m  accordng to your config;
@@ -18,27 +18,33 @@ if [ $step -le 0 ]; then
 
 fi
 
-feats_dir= #give the feature dir where you store your feats, it must includes {tr, cv, tt}_{inputs, labels} dirctories
+feats_dir=/home/disk1/snsun/Workspace/tensorflow/kaldi/data/wsj0/create-speaker-mixtures/feats_8k_czt/
+          #give the feature dir where you store your feats, it must includes {tr, cv, tt}_{inputs, labels} dirctories
 copy_labels=false
 
 if [ $step -le 1 ] ; then
-    echo " Feature format transformatio\n copy text ark to binary ark and scp and calculate the Mean and Variance for inputs." 
+    echo " Feature format transformatio \n copy text ark to binary ark and scp and calculate the Mean and Variance for inputs." 
+
     source path.sh #This is the Kaldi path file
-    for x in tr, cv, tt; do 
-        if copy_labels; then
-            for y in inputs, labels;do
+    for x in tr cv tt; do 
+        if $copy_labels; then
+            for y in inputs labels;do
                 copy-feats ark:$feats_dir/${x}_${y}/feats.txt ark,scp:$feats_dir/${x}_${y}/feats.ark,$feats_dir/${x}_${y}/feats.scp &
             done
-        else:
+        else
             for y in inputs; do
                 copy-feats ark:$feats_dir/${x}_${y}/feats.txt ark,scp:$feats_dir/${x}_${y}/feats.ark,$feats_dir/${x}_${y}/feats.scp &
             done
         fi
-        compute-cmvn ark:$feats_dir/${x}_inputs/feats.txt $feats_dir/${x}_${y}/cmvn.ark &
+#        compute-cmvn-stats ark:$feats_dir/${x}_inputs/feats.txt $feats_dir/${x}_${y}/cmvn.ark &
     done
     wait 
 fi
 
+if [ $step -le 2 ] ; then
+  echo "Transform the kaldi features to tf records"
+
+    
 
 
 
