91cbebbaf7a8c9c199de0e0b6de3e8de933304bd
snsun <snsun@nwpu-aslp.org>
2017-12-02 15:02:02 +0800

    Comment the pdb

--- run.sh
+++ run.sh
@@ -55,7 +55,7 @@ if [ $step -le 1 ]; then
     for x in tr tt cv; do
         find $tfrecords_dir/${x}_psm/ -iname "*.tfrecords" > $lists_dir/${x}_tf.lst
     done
-    tr_cmd="python -u -m pdb run_lstm.py \
+    tr_cmd="python -u run_lstm.py \
     --lists_dir=$lists_dir  --rnn_num_layers=$rnn_num_layers --batch_size=$batch_size --rnn_size=$rnn_size \
     --decode=$decode --learning_rate=$learning_rate --save_dir=$save_dir --data_dir=$data_dir --keep_prob=$keep_prob \
     --input_size=$input_size --output_size=$output_size  --assign=$assignment --resume_training=$resume_training \
