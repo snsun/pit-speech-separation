f6e39f07e20f2535121ef9af1585f0f12e459149
snsun <snsun@nwpu-aslp.org>
2017-12-03 11:13:14 +0800

    modify the loss showing stratage

--- run.sh
+++ run.sh
@@ -7,15 +7,14 @@
 #      We define the tf records format for our task, please see the codes for the details
 #   3. Traing & Test model: Tensorflow
 
-
-step=2
+step=1
 
 lists_dir=./lists/ #lists_dir is used to store some necessary files lists
 mkdir -p $lists_dir
 num_threads=12
 
 tfrecords_dir=data/tfrecords/
-gpu_id='0'
+gpu_id='4'
 TF_CPP_MIN_LOG_LEVEL=1
 rnn_num_layers=3
 tr_batch_size=32
@@ -24,7 +23,7 @@ tt_batch_size=1
 input_size=129
 output_size=129
 
-rnn_size=400
+rnn_size=496
 keep_prob=0.8
 learning_rate=0.0005
 halving_factor=0.7
@@ -57,11 +56,11 @@ if [ $step -le 1 ]; then
     
     echo "Start Traing RNN(LSTM or BLSTM) model."
     decode=0
-    batch_size=30
+    batch_size=25
     for x in tr tt cv; do
         find $tfrecords_dir/${x}_psm/ -iname "*.tfrecords" > $lists_dir/${x}_tf.lst
     done
-    tr_cmd="python -u run_lstm.py \
+    tr_cmd="/usr/bin/python2.7 -u run_lstm.py \
     --lists_dir=$lists_dir  --rnn_num_layers=$rnn_num_layers --batch_size=$batch_size --rnn_size=$rnn_size \
     --decode=$decode --learning_rate=$learning_rate --save_dir=$save_dir --data_dir=$data_dir --keep_prob=$keep_prob \
     --input_size=$input_size --output_size=$output_size  --assign=$assignment --resume_training=$resume_training \
@@ -70,7 +69,7 @@ if [ $step -le 1 ]; then
     echo $tr_cmd
     CUDA_VISIBLE_DEVICES=$gpu_id TF_CPP_MIN_LOG_LEVEL=$TF_CPP_MIN_LOG_LEVEL $tr_cmd
 fi
-#####################################################################################################
+    #####################################################################################################
 #   NOTE for STEP 2:                                                                              ###
 #       1. Make sure that you configure the RNN/data_dir/model_dir/ all rights                    ###
 #####################################################################################################
@@ -79,7 +78,7 @@ if [ $step -le 2 ]; then
     echo "Start Decoding."
     decode=1
     batch_size=30
-     tr_cmd="python -u  run_lstm.py --lists_dir=$lists_dir  --rnn_num_layers=$rnn_num_layers --batch_size=$batch_size --rnn_size=$rnn_size \
+     tr_cmd="/user/bin/python2.7 -u  run_lstm.py --lists_dir=$lists_dir  --rnn_num_layers=$rnn_num_layers --batch_size=$batch_size --rnn_size=$rnn_size \
     --decode=$decode --learning_rate=$learning_rate --save_dir=$save_dir --data_dir=$data_dir --keep_prob=$keep_prob \
     --input_size=$input_size --output_size=$output_size  --assign=$assignment --resume_training=$resume_training \
     --model_type=$model_type --czt_dim=128"
